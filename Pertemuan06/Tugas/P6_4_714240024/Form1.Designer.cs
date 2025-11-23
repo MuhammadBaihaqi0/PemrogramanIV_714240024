310aee888a963d2147b6aab6cd57319cbf41e436
MuhammadBaihaqi0 <baihaqisiregar09@gmail.com>
2025-11-23 20:23:15 +0700

    selesai

---
+++ Pertemuan06/Tugas/P6_4_714240024/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace P6_4_714240024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNPM = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtAlasan = new System.Windows.Forms.TextBox();
+            this.txtPanggilan = new System.Windows.Forms.TextBox();
+            this.txtSms = new System.Windows.Forms.TextBox();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNPM
+            // 
+            this.txtNPM.Location = new System.Drawing.Point(186, 71);
+            this.txtNPM.Name = "txtNPM";
+            this.txtNPM.Size = new System.Drawing.Size(130, 20);
+            this.txtNPM.TabIndex = 0;
+            this.txtNPM.Leave += new System.EventHandler(this.txtAngka_Leave);
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(186, 137);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(130, 20);
+            this.txtNama.TabIndex = 1;
+            this.txtNama.TextChanged += new System.EventHandler(this.txtNama_TextChanged);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(186, 209);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(130, 20);
+            this.txtUser.TabIndex = 2;
+            this.txtUser.TextChanged += new System.EventHandler(this.txtUser_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(533, 71);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(175, 20);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(186, 290);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(130, 20);
+            this.txtEmail.TabIndex = 4;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtRequired_TextChanged);
+            // 
+            // txtAlasan
+            // 
+            this.txtAlasan.Location = new System.Drawing.Point(533, 137);
+            this.txtAlasan.Name = "txtAlasan";
+            this.txtAlasan.Size = new System.Drawing.Size(175, 20);
+            this.txtAlasan.TabIndex = 5;
+            this.txtAlasan.TextChanged += new System.EventHandler(this.txtAlasan_TextChanged);
+            // 
+            // txtPanggilan
+            // 
+            this.txtPanggilan.Location = new System.Drawing.Point(533, 209);
+            this.txtPanggilan.Name = "txtPanggilan";
+            this.txtPanggilan.Size = new System.Drawing.Size(175, 20);
+            this.txtPanggilan.TabIndex = 6;
+            this.txtPanggilan.TextChanged += new System.EventHandler(this.txtPanggilan_TextChanged);
+            // 
+            // txtSms
+            // 
+            this.txtSms.Location = new System.Drawing.Point(533, 290);
+            this.txtSms.Name = "txtSms";
+            this.txtSms.Size = new System.Drawing.Size(175, 20);
+            this.txtSms.TabIndex = 7;
+            this.txtSms.TextChanged += new System.EventHandler(this.txtSms_TextChanged);
+            // 
+            // btnTampilkan
+            // 
+            this.btnTampilkan.Location = new System.Drawing.Point(211, 377);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(75, 23);
+            this.btnTampilkan.TabIndex = 8;
+            this.btnTampilkan.Text = "Tampilkan";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
+            this.btnTampilkan.Click += new System.EventHandler(this.btnTampilkan_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(522, 377);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Keluar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "NPM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nama Lengkap";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(408, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(408, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Nama panggilan";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(408, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Alasan Gabung";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(408, 297);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Tingkat Semester";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.DarkRed;
+            this.label9.Location = new System.Drawing.Point(256, 25);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(304, 18);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "  Forum Pendaftaran UKM Jago Coding";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnTampilkan);
+            this.Controls.Add(this.txtSms);
+            this.Controls.Add(this.txtPanggilan);
+            this.Controls.Add(this.txtAlasan);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtNPM);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNPM;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtAlasan;
+        private System.Windows.Forms.TextBox txtPanggilan;
+        private System.Windows.Forms.TextBox txtSms;
+        private System.Windows.Forms.Button btnTampilkan;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pertemuan06/Tugas/P6_4_714240024/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace P6_4_714240024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void  SetErrorMessages(TextBox textBox, string warningMessage, string wrongMessage, string correctMessage)
+        {
+            epWarning.SetError(textBox, warningMessage);
+            epWrong.SetError(textBox, wrongMessage);
+            epCorrect.SetError(textBox, correctMessage);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtAngka_Leave(object sender, EventArgs e)
+        {
+            if (txtNPM.Text == "")
+            {
+                SetErrorMessages(txtNPM, "Textbox NPM tidak boleh kosong!", "", "");
+            }
+            else if (txtNPM.Text.All(Char.IsNumber))
+            {
+                SetErrorMessages(txtNPM, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtNPM, "", "Inputan hanya boleh angka!", "");
+            }    
+        }
+
+        private void txtNama_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNama.Text == "")
+            {
+                SetErrorMessages(txtNama, "Textbox Nama tidak boleh kosong!", "", "");
+            }
+            else if (txtNama.Text.All(Char.IsLetter))
+            {
+                SetErrorMessages(txtNama, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtNama, "", "Inputan hanya boleh Huruf!", "");
+            }
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUser_TextChanged(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "")
+            {
+                SetErrorMessages(txtUser, "Textbox Username tidak boleh kosong!", "", "");
+            }
+            else if (txtUser.Text.All(Char.IsLetter))
+            {
+                SetErrorMessages(txtUser, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtUser, "", "Inputan hanya boleh Huruf!", "");
+            }
+        }
+
+        private void txtRequired_TextChanged(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == "")
+            {
+                SetErrorMessages(txtEmail, "Textbox Email tidak boleh kosong!", "", "");
+            }
+            else if (Regex.IsMatch(txtEmail.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                SetErrorMessages(txtEmail, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtEmail, "", "Format Email salah \nContoh: user@gmail.com!", "");
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                SetErrorMessages(txtPassword, "Textbox Password tidak boleh kosong!", "", "");
+            }
+            else if (txtPassword.Text.All(Char.IsLetter))
+            {
+                SetErrorMessages(txtPassword, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtPassword, "", "Inputan hanya boleh angka!", "");
+            }
+        }
+
+        private void txtAlasan_TextChanged(object sender, EventArgs e)
+        {
+            int minLength = 5;
+            int maxLength = 15;
+            int currentLength = txtAlasan.Text.Length;
+
+            if (txtAlasan.Text == "")
+            {
+                SetErrorMessages(txtAlasan, "Textbox Alasan tidak boleh kosong!", "", "");
+            }
+            else if (currentLength >= minLength && currentLength <= maxLength)
+            {
+                txtAlasan.SelectionStart = txtAlasan.Text.Length; 
+                SetErrorMessages(txtAlasan, "", "", "Panjang & Format OK!");
+            }
+            else
+            {
+                SetErrorMessages(txtAlasan, "", $"Panjang harus antara {minLength} dan {maxLength} karakter.", "");
+            }
+        }
+
+        private void txtPanggilan_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtPanggilan.Text == "")
+            {
+                SetErrorMessages(txtPanggilan, "Textbox panggilan tidak boleh kosong!", "", "");
+            }
+            else
+            {
+                txtPanggilan.Text = txtPanggilan.Text.ToUpper();
+                SetErrorMessages(txtPanggilan, "", "", "Format OK!");
+            }
+        }
+
+        private void txtSms_TextChanged(object sender, EventArgs e)
+        {
+            if (txtSms.Text == "")
+            {
+                SetErrorMessages(txtSms, "Textbox Tingkat Semester tidak boleh kosong!", "", "");
+            }
+            else
+            {
+                txtSms.Text = txtSms.Text.ToLower();
+                SetErrorMessages(txtSms, "", "", "Format OK!");
+            }
+        }
+
+        private void btnTampilkan_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+        "NPM : " + txtNPM.Text +
+        "\nNama Lengkap : " + txtNama.Text +
+        "\nUsername : " + txtUser.Text +
+        "\nEmail : " + txtEmail.Text +
+        "\nPassword : " + txtPassword.Text +
+        "\nNama Panggilan : " + txtAlasan.Text +
+        "\nAlasan Gabung : " + txtPanggilan.Text +
+        "\nTingkat Semester : " + txtSms.Text,
+        "Informasi Pendaftaran",
+        MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
