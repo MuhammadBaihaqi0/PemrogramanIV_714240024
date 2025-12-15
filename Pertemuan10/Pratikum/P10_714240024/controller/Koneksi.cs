bfae9f50f0da6ec820f7af44884f335d530b7330
Muhammad Baihaqi Siregar <baihaqisiregar09@gmail.com>
2025-12-15 16:05:15 +0700

    bismillah

---
+++ Pertemuan10/Pratikum/P10_714240024/controller/Koneksi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace P9_714240024
+{
+    internal class Koneksi
+    {
+        string connectionstring = "Server=localhost;Database=p9_pbo_;Uid=root;pwd=;";
+        MySqlConnection kon;
+
+        public void OpenConnection()
+        {
+            kon = new MySqlConnection(connectionstring);
+            kon.Open();
+        }
+
+        public void CloseConnection()
+        {
+            kon.Close();
+        }
+
+        public object ShowData(string query)
+        {
+            OpenConnection();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, kon);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            CloseConnection();
+            return table;
+        }
+
+        public void ExecuteQuery(MySqlCommand command)
+        {
+            command.Connection = kon;
+            command.ExecuteNonQuery();
+        }
+    }
+}
